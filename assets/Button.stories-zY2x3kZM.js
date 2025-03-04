342e12c0d4c15e456370117fcc2d3d604ba4ed11
11604075 <11604075@users.noreply.github.com>
2025-03-04 11:55:22 +0000

    Deploying to gh-pages from @ 11604075/storybook-CI-CD@8e703f689bc4e3fd9bb68924eb437162fed1f0df 🚀

---
+++ assets/Button.stories-zY2x3kZM.js
@@ -0,0 +1,17 @@
+import{j as h}from"./jsx-runtime-D_zvdyIk.js";function c(n){const{variant:E="primary",children:L,...N}=n;return h.jsx("button",{className:`button ${E}`,...N,children:L})}c.__docgenInfo={description:"",methods:[],displayName:"Button"};const I={title:"Form/Button",component:c,args:{children:"Button"}},s={args:{variant:"primary"}},e={args:{variant:"secondary"}},o={args:{variant:"success"}},t={args:{variant:"danger"}},D=n=>h.jsx(c,{...n}),r=D.bind({});r.args={variant:"primary"};const a=D.bind({});a.args={...r.args};var i,m,p;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(p=(m=s.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,u,g;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(g=(u=e.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var y,l,v;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    variant: 'success'
+  }
+}`,...(v=(l=o.parameters)==null?void 0:l.docs)==null?void 0:v.source}}};var S,B,x;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger'
+  }
+}`,...(x=(B=t.parameters)==null?void 0:B.docs)==null?void 0:x.source}}};var P,_,b;r.parameters={...r.parameters,docs:{...(P=r.parameters)==null?void 0:P.docs,source:{originalSource:"args => <Button {...args} />",...(b=(_=r.parameters)==null?void 0:_.docs)==null?void 0:b.source}}};var f,j,A;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:"args => <Button {...args} />",...(A=(j=a.parameters)==null?void 0:j.docs)==null?void 0:A.source}}};const O=["Primary","Secondary","Success","Danger","PrimaryA","LongPrimaryA"];export{t as Danger,a as LongPrimaryA,s as Primary,r as PrimaryA,e as Secondary,o as Success,O as __namedExportsOrder,I as default};
---
+++ assets/Input.stories-Bn6CO8AX.js
@@ -0,0 +1,14 @@
+import{j as I}from"./jsx-runtime-D_zvdyIk.js";function d(l){const{size:g="medium",...z}=l;return I.jsx("input",{className:`input ${g}`,...z})}d.__docgenInfo={description:"",methods:[],displayName:"Input"};const x={title:"Form/Input",component:d},e={args:{size:"small",placeholder:""}},s={args:{size:"medium"}},r={args:{size:"large"}};e.storyName="Small Input";var a,o,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    placeholder: ''
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var n,m,c;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(c=(m=s.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var i,p,u;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(u=(p=r.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const _=["Small","Medium","Large"];export{r as Large,s as Medium,e as Small,_ as __namedExportsOrder,x as default};
